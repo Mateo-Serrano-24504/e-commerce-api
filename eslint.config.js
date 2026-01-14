c98402d783df34949c0484deef182a5b3679d515
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-01-14 15:43:05 +0000

    build:
    Installed the newest version of the simple config package and ran dev-config

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import globals from 'globals'
+
+export default [
+  {
+    files: ['**/*.js', '**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-console': 'off',
+    },
+  },
+]
